1ac2e66679a7fe3f4cb3e828f92b1f24537b0670
William Kennedy <bill@ardanlabs.com>
2025-12-07 09:18:10 -0600

    fixing tests

--- tests/kronk_test.go
+++ tests/kronk_test.go
@@ -23,12 +23,12 @@ var (
 )
 
 func init() {
-	mpThinkToolChat = tools.MustFindModel(defaults.ModelsDir(""), "Qwen3-8B-Q8_0.gguf")
-	mpSimpleVision = tools.MustFindModel(defaults.ModelsDir(""), "Qwen2.5-VL-3B-Instruct-Q8_0.gguf")
-	mpEmbed = tools.MustFindModel(defaults.ModelsDir(""), "embeddinggemma-300m-qat-Q8_0.gguf")
+	mpThinkToolChat = tools.MustFindModel(defaults.ModelsDir(""), "Qwen3-8B-Q8_0")
+	mpSimpleVision = tools.MustFindModel(defaults.ModelsDir(""), "Qwen2.5-VL-3B-Instruct-Q8_0")
+	mpEmbed = tools.MustFindModel(defaults.ModelsDir(""), "embeddinggemma-300m-qat-Q8_0")
 
 	if os.Getenv("GITHUB_ACTIONS") != "true" {
-		mpGPTChat = tools.MustFindModel(defaults.ModelsDir(""), "gpt-oss-20b-Q8_0.gguf")
+		mpGPTChat = tools.MustFindModel(defaults.ModelsDir(""), "gpt-oss-20b-Q8_0")
 	}
 }
 
